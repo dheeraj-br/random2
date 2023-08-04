712d1c1142138e97ab8460424fd19b71f0f99c24
dheeraj <dheeraj.br2@gmail.com>
2023-08-04 18:13:33 +0530

    upadte index.js for testing

--- test/test2/test3/index.js
+++ test/test2/test3/index.js
@@ -2,17 +2,10 @@ const myArray = [1, 2, 3];
 const myObject = { key: 'value' };
 
 function myFunction(item) {
-  {
-  }
-  let nutused;
-  console.log('');
+  return item;
 }
 
 myArray.push(4);
 myObject.key = 'new value';
-
-console.log(myArray);
-
-console.log(myObject);
 
 myFunction(myArray[0]);
