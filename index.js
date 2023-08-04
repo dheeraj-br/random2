4744f87f2a05b7f1d8c78d7a41ac0bb64ad93058
dheeraj <dheeraj.br2@gmail.com>
2023-08-04 17:59:12 +0530

    add eslint, prettier, husky, lint-staged and their required configs

---
+++ index.js
@@ -0,0 +1,18 @@
+const myArray = [1, 2, 3];
+const myObject = { key: 'value' };
+
+function myFunction(item) {
+  {}
+  let nutused;
+  console.log('');
+}
+
+myArray.push(4);
+myObject.key = 'new value';
+
+console.log(myArray);
+
+console.log(myObject);
+
+myFunction(myArray[0]);
+             
